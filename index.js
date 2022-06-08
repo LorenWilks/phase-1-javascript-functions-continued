c869afa95fa74bceb89564a9ba28acc185bb347e
Loren Wilks <Loren.wilks@gmail.com>
2022-06-08 15:43:08 -0600

    commit tree 9ac01f7ce4d3d608e119996fdbe1239122842241

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(target = 'roller-skate') {
+    return `This Saturday, I want to ${target}!`
+}
+
+let mondayWork = function(mW = 'go to the office') {
+    return `This Monday, I will ${mW}.`
+}
+
+let wrapAdjective = function(flair = '*') {
+    return function(adj ='special') {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
